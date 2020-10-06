b75ccc6275bdb06565d8ec28a17b9d7aca8ca058
m4rz910 <tzavelisz@gmail.com>
2020-10-06 14:45:52 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NYISOToolkit-m4rz910",
+    version="0.0.1",
+    author="m4rz910",
+    author_email="viosimosllc@gmail.com",
+    description="A collection of modules for accessing power system data, generating statistics, and creating visualizations from the New York Independent System Operator (NYISO).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/m4rz910/NYISOToolkit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
