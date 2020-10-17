4e2a58c86a99169cc47bb7102e7d0cb0ae5658eb
m4rz910 <tzavelisz@gmail.com>
2020-10-16 21:04:27 -0400

    replaced hq net with new name

--- nyisotoolkit/nyisostat/nyisostat.py
+++ nyisotoolkit/nyisostat/nyisostat.py
@@ -37,7 +37,7 @@ class NYISOStat:
         load = NYISOData(dataset='load_5m',year=year).df.tz_convert('US/Eastern')['NYCA']
         fuel_mix = NYISOData(dataset='fuel_mix_5m',year=year).df.tz_convert('US/Eastern')
         imports = NYISOData(dataset='interface_flows_5m', year='2019').df.tz_convert('US/Eastern')
-        imports.drop(('External Flows', 'HQ NET', 'Flow (MW)'), axis='columns', inplace=True) #HQ Net is a subset of another external flow
+        imports.drop(('External Flows', 'SCH - HQ_IMPORT_EXPORT', 'Flow (MW)'), axis='columns', inplace=True) #HQ Net is a subset of another external flow
         imports = imports.loc[:,('External Flows',slice(None),'Flow (MW)')]
     
         #Energy Converstion [MWh] and Resampling By Summing Energy
