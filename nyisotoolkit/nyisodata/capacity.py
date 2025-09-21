6db0126eb7a854a417e651e1b71900e072f5003a
tzavelisz@gmail.com <tzavelisz@gmail.com>
2025-09-21 01:04:23 -0400

    added some proxy flexibility

--- nyisotoolkit/nyisodata/capacity.py
+++ nyisotoolkit/nyisodata/capacity.py
@@ -2,11 +2,13 @@ import pandas as pd
 import requests
 from pathlib import Path
 import pytz
+import os
+import time
 
 from nyisotoolkit.nyisodata import STORAGE_DIR
 
 class NYISOCapacity:
-    def __init__(self, date=pd.Timestamp.now()) -> None:
+    def __init__(self, date=pd.Timestamp.now(), wait=False) -> None:
         """
         Arguments:
             date (pandas.Timestamp): date that will be used to pull latest capacity
@@ -18,7 +20,11 @@ class NYISOCapacity:
         self.report_name = f"ICAP-Market-Report-{self.date.month_name()}-{self.date.year}.xlsx"
         
         self.exists = None # set in get_raw_data
-        self.get_raw_data()
+        
+        if wait:
+            pass
+        else:
+            self.get_raw_data()
 
     def get_url(self):
          
@@ -45,17 +51,27 @@ class NYISOCapacity:
         url = f"{capacity_market_base_url}/{report_name}"
         return url, report_name
 
-    def get_raw_data(self):
+    def get_raw_data(self, repull=False, **request_kwargs):
+        """Will determine whether whether the local reports pulled are usable and only pull new ones"""
+        
         url, report_name = self.get_url() # pull the latest file
-        r = requests.get(url)
         self.report_file = Path(self.download_dir, report_name)
-        if r.ok:
-            with open(self.report_file, 'wb') as file:
-                file.write(r.content)
+
+        # check does the local report file already exist locally
+        if self.report_file.exists() and (os.stat(self.report_file).st_size > 0) and (not repull):
             self.exists = True
-        else:
-            self.exists = False
-            print(f"Warning: Request failed for {url} with status: {r.status_code}")  # TODO: log this
+        else: # otherwise we have to pull the new files down
+            
+            r = requests.get(url, **request_kwargs)
+            
+            if (r.ok) and (len(r.content)>0): # Check if the content is not empty   
+                with open(self.report_file, 'wb') as file:
+                    file.write(r.content)
+                self.exists = True
+            else:
+                raise Exception(f"Request for {url} returned an empty response.")
+                self.exists = False
+                self.report_file.unlink(missing_ok=True)
             
     def prices(self):
         """Pull the most recent capacity market report's market clearing prices
@@ -65,20 +81,23 @@ class NYISOCapacity:
         """
         if not Path(self.download_dir, self.report_name).exists():
             self.get_raw_data(self)
-            
-        df = pd.read_excel(Path(self.download_dir, self.report_name),
-                           sheet_name="MCP Table", header=[0, 1]).iloc[:,:13]
-        df.rename(columns={"Unnamed: 0_level_0": "", "Date": ""},
-                  inplace=True)
-        df.set_index("", inplace=True)
-        df.index.name = 'datetime'
-        return df
+        
+        if self.exists:  
+            df = pd.read_excel(Path(self.download_dir, self.report_name),
+                            sheet_name="MCP Table", header=[0, 1]).iloc[:,:13]
+            df.rename(columns={"Unnamed: 0_level_0": "", "Date": ""},
+                    inplace=True)
+            df.set_index("", inplace=True)
+            df.index.name = 'datetime'
+            return df
+        else:
+            raise Exception(f'Data is not available - file is probably not available anymore on the website or its corrupted: {self.url}')
     
     def summary_table(self):
         if not Path(self.download_dir, self.report_name).exists():
             self.get_raw_data(self)
             
-        df = pd.read_excel(Path(self.download_dir, self.report_name),
+        df = pd.read_excel(Path(self.download_dir, self.report_name), engine='openpyxl',
                            sheet_name="Summary Table", header=[0, 1]).iloc[:,:17]
         df.rename(columns={"Unnamed: 0_level_0": "", "Date": ""},
                   inplace=True)
@@ -91,7 +110,7 @@ class NYISOCapacity:
         if not Path(self.download_dir, self.report_name).exists():
             self.get_raw_data(self)
             
-        df = pd.read_excel(Path(self.download_dir, self.report_name),
+        df = pd.read_excel(Path(self.download_dir, self.report_name), engine='openpyxl',
                            sheet_name="UCAP Table", header=[0, 1]).iloc[:,:17]
         df.rename(columns={"Unnamed: 0_level_0": "", "Date": ""},
                   inplace=True)
